dc3bba2190704d66008b7332bc97c8ed630f8d73
cungKring89 <cungringvirgoboy@yahoo.co.id>
2018-01-06 13:57:16 +0700

    Update Xperia to Kita.js

--- Xperia to Kita.js
+++ Xperia to Kita.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2018/01/firmware-lolipop-xperia-docomo.html"}
 if(x=="2"){xxx="/2018/01/cara-pasang-camera-cyberian.html"}
 if(x=="3"){xxx="/2018/01/cara-memperbaiki-sinyal-hilang-xperia.html"}
 if(x=="4"){xxx="/2018/01/pure-x-custome-rom-xperia-docomo.html"}
-if(x=="5"){xxx="/2018/01/perbedaan-sony-xperia-docomo-dan-global.html"}a_to_ve[i].href="https://convert-safelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2018/01/perbedaan-sony-xperia-docomo-dan-global.html"}a_to_ve[i].href="https://xperiakitasafelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
